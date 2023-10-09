019dd34f1cf00ba038fef65a5c6be2e4c3df6e21
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-09 15:18:07 +0330

    feat : add Middleware ForceJsonResponse

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\PaymentStatusEnum;
+use App\Enums\Payment\PaymentStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
