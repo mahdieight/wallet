009fc5afdc9b5bcfd45b02b7aadb68450c932608
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-15 23:12:29 +0330

    feat: add payment factory & seeder

---
+++ database/factories/PaymentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Payment\PaymentStatusEnum;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
+ */
+class PaymentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'unique_id' => uniqid(),
+            'user_id' => User::first(),
+            'status' => PaymentStatusEnum::PENDING->value,
+            'amount' => fake()->randomDigit,
+            'currency' => 'dollar',
+        ];
+    }
+}
