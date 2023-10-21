4eac12bd4aef4ab07688b19847e38aea50f85396
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-22 02:45:58 +0330

    feat: add new endpoint for create currency

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\CurrencyController;
 use App\Http\Controllers\API\V1\PaymentController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,6 @@ Route::prefix('v1')->group(function () {
     Route::get('payments/{payment}', [PaymentController::class, 'show']);
     Route::patch('payments/{payment}/reject', [PaymentController::class, 'reject']);
     Route::patch('payments/{payment}/approve', [PaymentController::class, 'approve']);
+
+    Route::post('currencies', [CurrencyController::class, 'store']);
 });
