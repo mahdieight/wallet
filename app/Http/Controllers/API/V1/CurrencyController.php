03168fc5ae4054b5804e6efa309e8a8243067a17
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-22 21:46:39 +0330

    feat: add new endpoint in currency for active/deactive currency

--- app/Http/Controllers/API/V1/CurrencyController.php
+++ app/Http/Controllers/API/V1/CurrencyController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Events\Currency\CurrencyActivated;
+use App\Events\Currency\CurrencyDeActivated;
 use App\Facades\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CurrencyStoreRequest;
 use App\Http\Resources\CurrencyResource;
 use App\Models\Currency;
 use Illuminate\Http\Request;
-
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 class CurrencyController extends Controller
 {
@@ -27,7 +29,6 @@ class CurrencyController extends Controller
     {
         $currencies = Currency::paginate(20);
         return Response::message('payment.messages.payment_list_found_successfully')->data(CurrencyResource::collection($currencies))->send();
-
     }
 
     /**
@@ -68,39 +69,33 @@ class CurrencyController extends Controller
     public function store(CurrencyStoreRequest $request)
     {
         $currency = Currency::create($request->all());
+
+        CurrencyActivated::dispatch($currency);
+
         return Response::message('currency.messages.currency_successfully_created')->data(new CurrencyResource($currency))->status(200)->send();
-
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+
+    public function active(Currency $currency)
     {
-        //
+        if ($currency->is_active)  throw new BadRequestException(__('currency.errors.currency_is_currently_active_and_cannot_be_reactivated'));
+
+        $currency->update(['is_active' => 1]);
+
+        CurrencyActivated::dispatch($currency);
+
+        Response::message('currency.messages.the_currency_has_been_activated_successfully')->data(new CurrencyResource($currency))->send();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function deActive(Currency $currency)
     {
-        //
-    }
+        if (!$currency->is_active)  throw new BadRequestException(__('currency.errors.currency_is_currently_inactive_and_cannot_be_reactivated'));
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $currency->update(['is_active' => 0]);
+
+        CurrencyDeActivated::dispatch($currency);
+
+        Response::message('currency.messages.the_currency_has_been_deactivated_successfully')->data(new CurrencyResource($currency))->send();
     }
 }
