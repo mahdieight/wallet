b6418ea473de1a592eedabae4471ffd00a96f43e
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-22 13:46:25 +0330

    feat: change order seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            CurrencySeeder::class,
             PaymentSeeder::class,
-            CurrencySeeder::class
         ]);
     }
 }
