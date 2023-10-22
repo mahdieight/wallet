7b5530d5eef9670d2003defb23a1bbc819ade776
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-22 17:27:54 +0330

    fix: show currency name in resource

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -17,7 +17,7 @@ class PaymentResource extends JsonResource
         return [
             'user' => $this->user,
             'amount' => $this->amount,
-            'currency_key' => $this->currency,
+            'currency' => $this->currency_key,
         ];
     }
 }
