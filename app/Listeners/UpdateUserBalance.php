325deae2830d3a3781ecd226cad9e5c2cea9179d
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-23 22:13:36 +0330

    feat: remove un use file from UpdateUserBalance

--- app/Listeners/UpdateUserBalance.php
+++ app/Listeners/UpdateUserBalance.php
@@ -3,8 +3,6 @@
 namespace App\Listeners;
 
 use App\Models\Transaction;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\DB;
 
 class UpdateUserBalance
