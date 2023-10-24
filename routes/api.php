621c197d6838ec7f1c569372fff8f3926082c84e
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-24 18:58:16 +0330

    feat: use reoute group in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,24 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    Route::post('payments', [PaymentController::class, 'store']);
-    Route::get('payments', [PaymentController::class, 'index']);
-    Route::get('payments/{payment}', [PaymentController::class, 'show']);
-    Route::patch('payments/{payment}/reject', [PaymentController::class, 'reject']);
-    Route::patch('payments/{payment}/approve', [PaymentController::class, 'approve']);
 
-    Route::get('currencies', [CurrencyController::class, 'index']);
-    Route::post('currencies', [CurrencyController::class, 'store']);
-    Route::patch('currencies/{currency}/active' , [CurrencyController::class , 'active']);
-    Route::patch('currencies/{currency}/deactive' , [CurrencyController::class , 'deActive']);
+    Route::prefix('payments')->group(function () {
+        Route::post('/', [PaymentController::class, 'store']);
+        Route::get('/', [PaymentController::class, 'index']);
+        Route::get('/{payment}', [PaymentController::class, 'show']);
+        Route::patch('/{payment}/reject', [PaymentController::class, 'reject']);
+        Route::patch('/{payment}/approve', [PaymentController::class, 'approve']);
+    });
 
-    Route::post('deposit/transfer' , [DepositController::class , 'transfer']);
+    Route::prefix('currencies')->group(function () {
+        Route::get('/', [CurrencyController::class, 'index']);
+        Route::post('/', [CurrencyController::class, 'store']);
+        Route::patch('/{currency}/active', [CurrencyController::class, 'active']);
+        Route::patch('/{currency}/deactive', [CurrencyController::class, 'deActive']);
+    });
+
+
+
+
+    Route::post('deposit/transfer', [DepositController::class, 'transfer']);
 });
