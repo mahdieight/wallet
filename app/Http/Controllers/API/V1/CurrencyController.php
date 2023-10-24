6bb6f69f72638fb37541a18c96380d76e3740cd6
Mahdi Amereh <mahdieightt@gmail.com>
2023-10-24 18:58:33 +0330

    fix: remove swagger annotation from CurrencyController

--- app/Http/Controllers/API/V1/CurrencyController.php
+++ app/Http/Controllers/API/V1/CurrencyController.php
@@ -14,58 +14,14 @@ use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 class CurrencyController extends Controller implements CurrencyControllerInterface
 {
-    /**
-     * @OA\Get(
-     *      path="/api/v1/currencies",
-     *      operationId="getCurrencyList",
-     *      tags={"Currency"},
-     *      summary="Get Currency List",
-     *      description="Returns currency list",
-     *      @OA\Response(response=201,description="Successful operation"),
-     *      @OA\Response(response=404, description="Payment List Not Found"),
-     * )
-     */
+
     public function index()
     {
         $currencies = Currency::paginate(20);
         return Response::message('payment.messages.payment_list_found_successfully')->data(CurrencyResource::collection($currencies))->send();
     }
 
-    /**
-     * @OA\Post(
-     *      path="/api/v1/currencies",
-     *      operationId="createCurrency",
-     *      tags={"Currency"},
-     *      summary="Create Currency",
-     *      description="Store a newly created resource in storage.",
-     *      @OA\Response(response=200,description="Currency Created"),
-     *      @OA\Response(response=400, description="Bad request"),
-     *      @OA\Parameter(
-     *         name="key",
-     *         in="query",
-     *         required=true,
-     *         description="key currency",
-     *     ),
-     *      @OA\Parameter(
-     *         name="name",
-     *         in="query",
-     *         required=true,
-     *         description="name currency",
-     *     ),
-     *      @OA\Parameter(
-     *         name="symbol",
-     *         in="query",
-     *         required=true,
-     *         description="symbol currency",
-     *     ),
-     *      @OA\Parameter(
-     *         name="iso_code",
-     *         in="query",
-     *         required=true,
-     *         description="symbol currency",
-     *     ),
-     * )
-     */
+    
     public function store(CurrencyStoreRequest $request)
     {
         $currency = Currency::create($request->all());
@@ -76,25 +32,6 @@ class CurrencyController extends Controller implements CurrencyControllerInterfa
     }
 
 
-
-    /**
-     * @OA\Patch(
-     *      path="/api/v1/currencies/{key}/active",
-     *      operationId="activeCurrency",
-     *      tags={"Currency"},
-     *      summary="Active Currency",
-     *      description="Active Currency",
-     *      @OA\Response(response=201,description="Currency Successfully Activated"),
-     *      @OA\Response(response=403, description="Bad request"),
-     *      @OA\Response(response=404, description="Not Found"),
-     *      @OA\Parameter(
-     *         description="currency key",
-     *         in="path",
-     *         name="key",
-     *         required=true,
-     *     ),
-     * )
-     */
     public function active(Currency $currency)
     {
         if ($currency->is_active)  throw new BadRequestException(__('currency.errors.currency_is_currently_active_and_cannot_be_reactivated'));
@@ -107,24 +44,6 @@ class CurrencyController extends Controller implements CurrencyControllerInterfa
     }
 
 
-    /**
-     * @OA\Patch(
-     *      path="/api/v1/currencies/{key}/deactive",
-     *      operationId="deactiveCurrency",
-     *      tags={"Currency"},
-     *      summary="DeActive Currency",
-     *      description="DeActive Currency",
-     *      @OA\Response(response=201,description="Currency Successfully DeActivated"),
-     *      @OA\Response(response=403, description="Bad request"),
-     *      @OA\Response(response=404, description="Not Found"),
-     *      @OA\Parameter(
-     *         description="currency key",
-     *         in="path",
-     *         name="key",
-     *         required=true,
-     *     ),
-     * )
-     */
     public function deActive(Currency $currency)
     {
         if (!$currency->is_active)  throw new BadRequestException(__('currency.errors.currency_is_currently_inactive_and_cannot_be_reactivated'));
